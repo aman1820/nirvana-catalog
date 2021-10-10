f0c88cf9ead1da765a717960a630b6bb00f1b9d7
Aman Gupta <aman.gupta@catalystone.com>
2021-10-10 23:03:13 +0530

    Initial commit

---
+++ src/main/java/com/nirvana/nirvanacatalog/NirvanaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.nirvana.nirvanacatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirvanaCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirvanaCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirvana/nirvanacatalog/NirvanaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirvana.nirvanacatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NirvanaCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
